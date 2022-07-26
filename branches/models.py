4ed28633406eac271829bea523c1da307ef6be1e
fahad alobaid <fahad.n.b.alobaid@gmail.com>
2022-07-26 18:11:13 +0300

    Endtask

--- branches/models.py
+++ branches/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ImageField
 
 
 class Restaurant(models.Model):
@@ -20,7 +21,10 @@ class Employee(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
+    
+  
     )
+    avatar = models.ImageField(upload_to='upload', null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
